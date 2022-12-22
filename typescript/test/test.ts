b7ba1c2f2a4ae4727b72748ca9c04522a92fd67d
Jimmy Chu <jimmychu0807@gmail.com>
2022-12-22 11:29:21 +0800

    basic setup

---
+++ typescript/test/test.ts
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('Basic Tests', () => {
+  describe('Test threshold 1 with 5 of 7 shares of a group combinations', () => {
+    // const mnemonics = slip.fromPath('r/0').mnemonics;
+    expect(1).to.eq(1);
+  });
+});
